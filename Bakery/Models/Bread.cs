e7aa585c84ac37cb86a77f970ab15f0821745d91
Patrick Osten <Patrickosten040@gmail.com>
2020-12-11 08:56:08 -0800

    add untracked files for commit

---
+++ Bakery/Models/Bread.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Item
+{
+  public class Bread
+  {
+    public int Order { get; set; }
+    public Bread(int order)
+    {
+      Order = order;
+    }
+  }
+}
---
+++ Bakery/Models/Pastry.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Item
+{
+  public class Pastry
+  {
+    public int Order { get; set; }
+    public Pastry(int order)
+    {
+      Order = order;
+    }
+  }
+}
